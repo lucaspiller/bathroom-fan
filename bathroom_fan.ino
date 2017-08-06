d705d1b2546e10c919d17316e60b98e87ed023cd
Luca Spiller <luca@stackednotion.com>
2017-08-06 10:43:36 +0300

    Add OTA update

--- bathroom_fan.ino
+++ bathroom_fan.ino
@@ -2,6 +2,9 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
 
 #include "config.h"
 
@@ -365,6 +368,42 @@ void serverStart() {
 }
 
 //
+// OTA Update
+//
+void otaStart() {
+  ArduinoOTA.setHostname("esp8266-bathroom-fan");
+  ArduinoOTA.onStart([]() {
+    fanStop();
+
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH)
+      type = "sketch";
+    else // U_SPIFFS
+      type = "filesystem";
+
+    // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+    Serial.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+    delay(1000);
+    ESP.restart();
+  });
+  ArduinoOTA.begin();
+}
+
+//
 // Main Program
 //
 void setup() {
@@ -392,6 +431,7 @@ void setup() {
 
   // Start
   serverStart();
+  otaStart();
 }
 
 void loop() {
@@ -414,5 +454,6 @@ void loop() {
   }
 
   server.handleClient();
+  ArduinoOTA.handle();
   delay(100);
 }
